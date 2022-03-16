5c0e2168a75cddf318f940aad1441f0b111c8e86
Diana <kozachenkodiana03@mail.ru>
2022-03-16 13:26:24 +0700

    Added render genres

--- KPNoYandexV/KPNoYandexV/View/Genres.xaml.cs
+++ KPNoYandexV/KPNoYandexV/View/Genres.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using KPNoYandexV.Data;
+using KPNoYandexV.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,48 @@ namespace KPNoYandexV.View
         public Genres()
         {
             InitializeComponent();
+
+            List<Genre> Genres = new List<Genre>();
+            using (var db = new KPNoYandexVContext())
+            {
+                Genres = db.Genres.ToList();
+            }
+
+            foreach (var Genre in Genres)
+            {
+                var Block = new Rectangle();
+                Block.Width = 300;
+                Block.Height = 50;
+                Block.Stroke = Brushes.Gray;
+                Block.RadiusX = 10;
+                Block.RadiusY = 10;
+                Block.StrokeThickness = 1;
+                Block.HorizontalAlignment = HorizontalAlignment.Center;
+                Block.VerticalAlignment = VerticalAlignment.Stretch;
+                Block.Margin = new Thickness(0, 0, 0, 20);
+
+                var Name = new TextBlock();
+                Name.MaxWidth = 200;
+                Name.TextWrapping = TextWrapping.Wrap;
+                Name.FontFamily = new FontFamily("Consolas");
+                Name.FontSize = 16;
+                Name.Margin = new Thickness(0, -55, 0, 0);
+                Name.Text = Genre.Name;
+
+                var Number = new TextBlock();
+                Number.MaxWidth = 200;
+                Number.TextWrapping = TextWrapping.Wrap;
+                Number.FontFamily = new FontFamily("Consolas");
+                Number.FontSize = 16;
+                Number.Margin = new Thickness(-60, -55, 0, 0);
+                Number.Text = Genre.Id.ToString();
+
+
+                GenresStackPanel.Children.Add(Block);
+                GenresStackPanel.Children.Add(Name);
+                GenresStackPanel.Children.Add(Number);
+
+            }
         }
     }
 }
---
+++ KPNoYandexV/KPNoYandexV/ViewModel/GenresPageVM.cs
@@ -0,0 +1,37 @@
+﻿using KPNoYandexV.Data;
+using KPNoYandexV.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPNoYandexV.ViewModel
+{
+    public class GenresPageVM : INotifyPropertyChanged
+    {
+        private List<Genre> genres;
+
+        public List<Genre> Genres { get { return genres; } set { genres = value; OnPropertyChanged(); } }
+
+        public GenresPageVM()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void GetAllFilms()
+        {
+            using (var db = new KPNoYandexVContext())
+            {
+                Genres = db.Genres.ToList();
+            }
+        }
+    }
+}
