8301ef5e5ba32376f4142dce18ecdec0e8382b3e
Diana <kozachenkodiana03@mail.ru>
2022-03-16 13:13:13 +0700

    Added ouput films from db

---
+++ KPNoYandexV/KPNoYandexV/ViewModel/FilmsPageVM.cs
@@ -0,0 +1,40 @@
+﻿using KPNoYandexV.Data;
+using KPNoYandexV.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPNoYandexV.ViewModel
+{
+    public class FilmsPageVM : INotifyPropertyChanged
+    {
+        private List<Film> films;
+
+        public List<Film> Films { get { return films; } set { films = value; OnPropertyChanged(); } }
+
+        public FilmsPageVM()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void GetAllFilms()
+        {
+            using (var db = new KPNoYandexVContext())
+            {
+                Films = db.Films.ToList();
+            }
+        }
+
+
+        
+    }
+}
