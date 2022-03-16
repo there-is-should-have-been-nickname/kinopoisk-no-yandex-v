a5957175c07a121979f53005a91a4e711b428f97
Diana <kozachenkodiana03@mail.ru>
2022-03-16 13:35:08 +0700

    Added render actors

---
+++ KPNoYandexV/KPNoYandexV/ViewModel/ActorsPageVM.cs
@@ -0,0 +1,37 @@
+﻿using KPNoYandexV.Data;
+using KPNoYandexV.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPNoYandexV.ViewModel
+{
+    public class ActorsPageVM : INotifyPropertyChanged
+    {
+        private List<Actor> actors;
+
+        public List<Actor> Actors { get { return actors; } set { actors = value; OnPropertyChanged(); } }
+
+        public ActorsPageVM()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+
+        public void GetAllFilms()
+        {
+            using (var db = new KPNoYandexVContext())
+            {
+                Actors = db.Actors.ToList();
+            }
+        }
+    }
+}
