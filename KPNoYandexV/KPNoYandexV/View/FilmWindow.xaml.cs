d38ac184eb95da718f224e3b5eb5b90d9a273989
Diana <kozachenkodiana03@mail.ru>
2022-03-17 19:36:38 +0700

    Added info about film, added poster

--- KPNoYandexV/KPNoYandexV/View/FilmWindow.xaml.cs
+++ KPNoYandexV/KPNoYandexV/View/FilmWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using KPNoYandexV.ViewModel;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,7 +24,7 @@ namespace KPNoYandexV.View
         {
             InitializeComponent();
 
-            
+            DataContext = new FilmWindowVM(FilmId);
         }
     }
 }
---
+++ KPNoYandexV/KPNoYandexV/ViewModel/FilmWindowVM.cs
@@ -0,0 +1,43 @@
+﻿using KPNoYandexV.Data;
+using KPNoYandexV.Model;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KPNoYandexV.ViewModel
+{
+    class FilmWindowVM : INotifyPropertyChanged
+    {
+        private Film film;
+        private string yearAndCounry;
+        private string ratingAndRevNumber;
+        private string path;
+
+        public Film CurrentFilm { get { return film; } set { film = value; OnPropertyChanged(); } }
+        public string YearAndCounry { get { return yearAndCounry; } set { yearAndCounry = value; OnPropertyChanged(); } }
+        public string RatingAndRevNumber { get { return ratingAndRevNumber; } set { ratingAndRevNumber = value; OnPropertyChanged(); } }
+        public string Path { get { return path; } set { path = value; OnPropertyChanged(); } }
+
+        public FilmWindowVM(int Id)
+        {
+            using (KPNoYandexVContext context = new KPNoYandexVContext())
+            {
+                CurrentFilm = context.Films.SingleOrDefault(film => film.Id == Id);
+            }
+            DateTime Date = Convert.ToDateTime(CurrentFilm.Year.ToString());
+            YearAndCounry = Date.Year.ToString() + ", " + CurrentFilm.Country;
+            RatingAndRevNumber = CurrentFilm.Rating + "; отзывов:" + CurrentFilm.ReviewsNumber;
+            Path = $"C:\\Users\\ACER\\Desktop\\Projects\\kinopoisk-no-yandex-v\\KPNoYandexV\\KPNoYandexV\\Images\\Posters\\{CurrentFilm.PosterPath}";
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
