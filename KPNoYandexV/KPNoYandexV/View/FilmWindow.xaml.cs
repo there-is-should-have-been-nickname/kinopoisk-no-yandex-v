8e40910f01b010728e7a0c241ece791945cff46d
Diana <kozachenkodiana03@mail.ru>
2022-03-22 21:24:08 +0700

    Added render all actors in film

--- KPNoYandexV/KPNoYandexV/View/FilmWindow.xaml.cs
+++ KPNoYandexV/KPNoYandexV/View/FilmWindow.xaml.cs
@@ -23,8 +23,36 @@ namespace KPNoYandexV.View
         public FilmWindow(int FilmId)
         {
             InitializeComponent();
+            var ViewModel = new FilmWindowVM(FilmId);
 
-            DataContext = new FilmWindowVM(FilmId);
+            DataContext = ViewModel;
+
+            foreach (var Act in ViewModel.CurrentActors)
+            {
+                var Block = new Rectangle();
+                Block.Width = 115;
+                Block.Height = 30;
+                Block.Stroke = Brushes.Gray;
+                Block.RadiusX = 10;
+                Block.RadiusY = 10;
+                Block.StrokeThickness = 1;
+                Block.HorizontalAlignment = HorizontalAlignment.Center;
+                Block.VerticalAlignment = VerticalAlignment.Stretch;
+                Block.Margin = new Thickness(0, 0, 20, 0);
+
+                var Name = new TextBlock();
+                Name.MaxWidth = 150;
+                Name.TextWrapping = TextWrapping.Wrap;
+                Name.FontFamily = new FontFamily("Consolas");
+                Name.FontSize = 12;
+                Name.Margin = new Thickness(-120, 15, 0, 0);
+                Name.Text = Act.FirstName + " " + Act.LastName;
+
+                ActorsStackPanel.Children.Add(Block);
+                ActorsStackPanel.Children.Add(Name);
+
+
+            }
         }
     }
 }
--- KPNoYandexV/KPNoYandexV/ViewModel/FilmWindowVM.cs
+++ KPNoYandexV/KPNoYandexV/ViewModel/FilmWindowVM.cs
@@ -1,5 +1,6 @@
 ﻿using KPNoYandexV.Data;
 using KPNoYandexV.Model;
+using KPNoYandexV.View;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -12,21 +13,31 @@ namespace KPNoYandexV.ViewModel
 {
     class FilmWindowVM : INotifyPropertyChanged
     {
-        private Film film;
+        private Film film = new Film();
         private string yearAndCounry;
         private string ratingAndRevNumber;
         private string path;
+        private List<Actor> actors = new List<Actor>();
+
 
         public Film CurrentFilm { get { return film; } set { film = value; OnPropertyChanged(); } }
         public string YearAndCounry { get { return yearAndCounry; } set { yearAndCounry = value; OnPropertyChanged(); } }
         public string RatingAndRevNumber { get { return ratingAndRevNumber; } set { ratingAndRevNumber = value; OnPropertyChanged(); } }
         public string Path { get { return path; } set { path = value; OnPropertyChanged(); } }
+        public List<Actor> CurrentActors { get { return actors; } set { actors = value; OnPropertyChanged(); } }
 
         public FilmWindowVM(int Id)
         {
             using (KPNoYandexVContext context = new KPNoYandexVContext())
             {
                 CurrentFilm = context.Films.SingleOrDefault(film => film.Id == Id);
+                List<FilmsActor> FilmActors = context.FilmsActors.Where(filmact => filmact.FilmId == CurrentFilm.Id).ToList();
+                foreach (var FilmActor in FilmActors)
+                {
+                    Actor Act = context.Actors.SingleOrDefault(act => act.Id == FilmActor.ActorId);
+                    CurrentActors.Add(Act);
+                }
+
             }
             DateTime Date = Convert.ToDateTime(CurrentFilm.Year.ToString());
             YearAndCounry = Date.Year.ToString() + ", " + CurrentFilm.Country;
