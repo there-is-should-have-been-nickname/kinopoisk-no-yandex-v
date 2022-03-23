6ea2175cb9a6bbf31ed06cf2161b71df7202db17
Diana <kozachenkodiana03@mail.ru>
2022-03-23 16:40:11 +0700

    Added opening actor page on film page

--- KPNoYandexV/KPNoYandexV/View/FilmWindow.xaml.cs
+++ KPNoYandexV/KPNoYandexV/View/FilmWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
@@ -28,28 +29,31 @@ namespace KPNoYandexV.View
 
             foreach (var Act in ViewModel.CurrentActors)
             {
-                var Block = new Rectangle();
-                Block.Width = 115;
-                Block.Height = 30;
-                Block.Stroke = Brushes.Gray;
-                Block.RadiusX = 10;
-                Block.RadiusY = 10;
-                Block.StrokeThickness = 1;
-                Block.HorizontalAlignment = HorizontalAlignment.Center;
-                Block.VerticalAlignment = VerticalAlignment.Stretch;
-                Block.Margin = new Thickness(0, 0, 20, 0);
+                var TemplateButton = new ControlTemplate(typeof(Button));
+                var Bord = new FrameworkElementFactory(typeof(Border));
+                Bord.SetValue(Border.WidthProperty, 115.0);
+                Bord.SetValue(Border.HeightProperty, 30.0);
+                Bord.SetValue(Border.BorderBrushProperty, Brushes.Gray);
+                Bord.SetValue(Border.BorderThicknessProperty, new Thickness(1, 1, 1, 1));
+                Bord.SetValue(Border.CornerRadiusProperty, new CornerRadius(10.0));
+                Bord.SetValue(Border.BackgroundProperty, Brushes.Transparent);
+                TemplateButton.VisualTree = Bord;
+                var ContentPresent = new FrameworkElementFactory(typeof(ContentPresenter));
+                Bord.AppendChild(ContentPresent);
 
-                var Name = new TextBlock();
-                Name.MaxWidth = 150;
-                Name.TextWrapping = TextWrapping.Wrap;
-                Name.FontFamily = new FontFamily("Consolas");
-                Name.FontSize = 12;
-                Name.Margin = new Thickness(-120, 15, 0, 0);
-                Name.Text = Act.FirstName + " " + Act.LastName;
 
-                ActorsStackPanel.Children.Add(Block);
-                ActorsStackPanel.Children.Add(Name);
+                var Btn = new Button();
+                Btn.Template = TemplateButton;
+                Btn.Margin = new Thickness(10, 15, 0, 0);
+                
+                Btn.HorizontalContentAlignment = HorizontalAlignment.Center;
+                Btn.VerticalContentAlignment = VerticalAlignment.Center;
+                Btn.Content = Act.FirstName + " " + Act.LastName;
+                Btn.Cursor = Cursors.Hand;
+                Btn.Command = ViewModel.ActorOpenClick;
+                Btn.CommandParameter = Act.Id;
 
+                ActorsStackPanel.Children.Add(Btn);
 
             }
         }
